87958e926a7660353066ab9707323437794d51b3
Ivan Thrysøe <ivanvt@gmail.com>
2022-04-24 00:47:18 +0200

    first commit

---
+++ Game.cs
@@ -0,0 +1,20 @@
+using DIKUArcade;
+using DIKUArcade.GUI;
+
+namespace Breakout
+{
+    public class Game : DIKUGame
+    {
+        public Game(WindowArgs windowArgs) : base(windowArgs) {
+
+        }
+
+        public override void Update() {
+
+        }
+
+        public override void Render() {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using DIKUArcade.GUI;
+
+namespace Breakout {
+    public class Program {
+
+        static void Main(string[] args)
+        {
+            var windowArgs = new WindowArgs() { Title = "Breakout" };
+            Game game = new Game(windowArgs);
+        }
+    }
+}
